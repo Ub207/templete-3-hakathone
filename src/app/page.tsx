17b6ee8deb8e9e159ec0a1438fdcbabd4dea51ce
Sid-Taha <twahasiddqui11@gmail.com>
2024-12-08 04:58:58 +0500

    up3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HelloNikeApp from "@/components/HelloNikeApp";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="mt-[99px]">
+      <HelloNikeApp/>
+    </div>
   );
 }
