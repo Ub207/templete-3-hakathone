1a5414ffc3921aa113d050b7cb596c0085b3f8e6
Sid-Taha <twahasiddqui11@gmail.com>
2024-12-08 05:54:10 +0500

    up4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import HelloNikeApp from "@/components/HelloNikeApp";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="mt-[99px]">
       <HelloNikeApp/>
+
+      <div className="px-[48px]">
+        <Hero/>
+        <CardSection/>
+
+      </div>
     </div>
   );
 }
